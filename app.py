5353dba123ad54a13c36b847356c3010c91ed39e
Knut Hühne <knut@k-nut.eu>
2017-06-19 12:23:57 +0200

    improve formatting

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ from jinja2 import evalcontextfilter, Markup, escape
 
 app = Flask(__name__)
 
+
 @app.route("/session/<session>")
 def protocol(session):
     conn = sqlite3.connect(os.environ.get("DB_URL"))
 
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    data = c.execute('select sequence, speaker_cleaned, speaker_party, type, text from de_bundestag_plpr where sitzung=?;', [session])
+    data = c.execute('select sequence, speaker_cleaned, speaker_party, type, text'
+                     'from de_bundestag_plpr '
+                     'where sitzung=?;', [session])
     return render_template('protocol.html', data=data)
+
 
 @app.template_filter()
 @evalcontextfilter
@@ -22,7 +26,6 @@ def nl2br(eval_ctx, value):
     _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
     result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
         for p in _paragraph_re.split(escape(value)))
-    print(value)
     if eval_ctx.autoescape:
         result = Markup(result)
     return result
